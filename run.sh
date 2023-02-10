77cecdb695c61c68f4306f53ece4a388ff719e3c
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2023-02-10 19:11:26 +0100

    Use 2 CPUS

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ xhost +$(hostname)
 
 #docker run -ti --rm -e DISPLAY=host.docker.internal:0 -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
 #docker run -ti --rm -e DISPLAY=$DISPLAY -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
-docker run -ti --rm -e DISPLAY=host.docker.internal:0 -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
+docker run -ti --rm --cpus 2.0 -e DISPLAY=host.docker.internal:0 -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
