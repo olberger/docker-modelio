003152cb8e922d44859fb3bcf2a7b114d594f8c5
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2023-02-10 18:57:14 +0100

    Tested on Ventura 13.0
    YMMV

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 # Check https://gist.github.com/paul-krohn/e45f96181b1cf5e536325d1bdee6c949 and adjust the followin script until it works. Pull requests welcome
 
 #xhost + 127.0.0.1
-xhost +$(hostname).local
+xhost +$(hostname)
 
 #docker run -ti --rm -e DISPLAY=host.docker.internal:0 -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
-docker run -ti --rm -e DISPLAY=$DISPLAY -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
+#docker run -ti --rm -e DISPLAY=$DISPLAY -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
+docker run -ti --rm -e DISPLAY=host.docker.internal:0 -v $HOME/.modelio:/home/modelio/.modelio:z -v $HOME/modelio:/home/modelio/modelio:z --net=host --ipc=host olberger/docker-modelio:4.1 bash
